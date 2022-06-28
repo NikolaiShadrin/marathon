2ef5aa3039a4dfb336dcbd94875e8e81804a4139
NikolaiShadrin <shyure@bk.ru>
2022-06-28 16:53:29 +0300

    Last 3-4 task day 4

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,36 @@
 package day4;
 
+import java.util.Random;
+
 public class Task3 {
     public static void main(String[] args) {
+        int [] [] array = new int[12][8];
+
+        Random random = new Random();
+
+        for (int i=0; i<array.length; i++) {
+            for (int k=0; k<array[i].length; k++) {
+                array[i][k] = random.nextInt(50);
+
+            }
+        }
+
+        int maxZnach = 0;
+        for (int i=0; i< array.length; i++) {
+            int sum = 0;
+
+            for(int k=0; k<array[i].length; k++) {
+                sum = sum + array[i][k];
+            }
+
+            if (sum>maxZnach) {
+                maxZnach = sum;
+            }
+        }
+
+        System.out.println(maxZnach);
+
+
 
     }
 }
--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,34 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task4 {
     public static void main(String[] args) {
+        int[] array = new int[100];
+
+        Random rdm = new Random();
+
+        for (int i=0; i< array.length; i++) {
+            array[i] = rdm.nextInt(10000);
+        }
+        System.out.println(Arrays.toString(array));
+
+        int maxSum = 0;
+        int maxSumIn = 0;
+        for (int k=0; k< array.length; k++) {
+            int sumFirstThree = 0;
+            for (int i=k; i<k+3; i++) {
+                sumFirstThree = sumFirstThree + array[i];
+            }
+
+            if (sumFirstThree>maxSum) {
+                maxSumIn = maxSum;
+                maxSumIn = k;
+            }
+
+            }
+
 
     }
 }
