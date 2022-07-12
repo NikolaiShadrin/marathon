efac84dd0879cc08d4fe53d47fbd7e49f6a80bc3
NikolaiShadrin <shyure@bk.ru>
2022-07-12 19:30:02 +0300

    Day 15 / Test 1-2

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,40 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("src/main/java/day16/Doc1.txt");
+
+        Task1.printResult(file);
+
+
+    }
+
+    public static void printResult(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String str = scanner.nextLine();
+            String[] array = str.split(" ");
+
+            int sum = 0;
+            double result = 0;
+
+            for (String number : array) {
+                sum = sum + Integer.parseInt(number);
+            }
+
+            result = (double)sum/array.length;
+
+            System.out.println(String.format(result + " --> %.3f",result));
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
 
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,67 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Random random = new Random();
+        File file = new File("src/main/java/day16/fi.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Файл file1 не удалось создать");
+        }
+
+        File file1 = new File("src/main/java/day16/fi2.txt");
+        try {
+            file1.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Файл file2 не удалось создать ");
+        }
+
+        PrintWriter pw = new PrintWriter(file);
+
+        for (int i = 0; i<1000; i++) {
+            pw.println(random.nextInt(100));
+
+        }
+
+        pw.close();
+
+        Scanner scanner = new Scanner(file);
+        PrintWriter pw2 = new PrintWriter(file1);
+        int sum = 0;
+        int counter = 0;
+
+        while (scanner.hasNextLine()) {
+            sum = sum + Integer.parseInt(scanner.nextLine());
+            counter++;
+
+            if (counter ==20) {
+                pw2.println(sum/20.0);
+                counter = 0;
+                sum = 0;
+            }
+        }
+
+        pw2.close();
+
+        Task2.printResult(file1);
+    }
+
+    public static void printResult(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        double sum = 0;
+
+        while (scanner.hasNextLine()) {
+            sum = sum + Double.parseDouble(scanner.nextLine());
+        }
+
+        System.out.println((int)sum);
     }
 }
