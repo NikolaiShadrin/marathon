3485b0f2b74e0e8b0b4f98e3427a766c61e85cc2
NikolaiShadrin <shyure@bk.ru>
2022-07-14 20:53:16 +0300

    Day 18 / Test 1-2

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,19 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(count7(717771237)); // 5
 
+    }
+
+    public static int count7(int s) {
+        if (s == 0) {
+            return 0;
+        }
+
+        if (s % 10 == 7) {
+            return 1 + count7(s/10);
+        } else {
+            return count7(s/10);
+        }
     }
 }
